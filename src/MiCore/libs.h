e645b4301360d4a643379a06346efd458c2f3324
NewTest <newtest@Dmitris-iMac.local>
2021-11-27 21:05:41 -0500

    moved necessary includes to a seperate header

---
+++ src/MiCore/libs.h
@@ -0,0 +1,19 @@
+/* Including required libraries */
+/* C++ built-in libraries */
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <vector>
+#include <string>
+
+/* 3rd Party libraries */
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+/* Custom libraries */
+#include "extern.h"
+// MATH
+#include "../math/vector.h"
+#include "../math/matrix.h"
+#include "../math/noise.h"
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,4 @@
-/* Including required libraries */
-/* C++ built-in libraries */
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <map>
-#include <vector>
-#include <string>
-
-/* 3rd Party libraries */
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-
-/* Custom libraries */
-#include "MiCore/extern.h"
-// MATH
-#include "math/vector.h"
-#include "math/matrix.h"
-#include "math/noise.h"
+#include "MiCore/libs.h"
 // ENGINE
 #include "MiCore/engine.h"
 #include "scenes/main_scene.h"
